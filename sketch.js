a2b37114a52e40a7469ebe100468428a3f7d4481
buz <emrah8@gmail.com>
2025-04-27 21:29:43 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -263,7 +263,9 @@ function setup() {
   
   let canvasWidth = 840;
   let canvasHeight = 870;
-  createCanvas(canvasWidth, canvasHeight);
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  let ctx = canvas.drawingContext;
+  ctx.willReadFrequently = true;
   tableBuffer = createGraphics(200, 625);
 
   precomputeWolfPoints();
